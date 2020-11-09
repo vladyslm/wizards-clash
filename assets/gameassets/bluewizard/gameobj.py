d84a7f750e851d781e13a7470c8122c40b86ae6a
vladislav <vladislav.mpm@gmail.com>
2020-11-09 18:36:34 +0100

    modify: clean up the old code

--- assets/gameassets/bluewizard/gameobj.py
+++ assets/gameassets/bluewizard/gameobj.py
@@ -2,15 +2,6 @@ import pygame
 import os
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
-# # tmp = os.path.join("fight-anim")
-# # print(tmp)
-# # print(os.path.abspath(os.getcwd()))
-# path = os.path.abspath(os.getcwd())
-# # /home/vladislav/Projects/python/wizards-clash/assets/gameassets/bluewizard/idle-anim
-# # tmp2 = pygame.image.load(f"{path}/assets/gameassets/bluewizard/idle-anim/idle_1.png")
-# tmp2 = os.path.dirname(os.path.abspath(__file__))
-# tmp_file = pygame.image.load(f"{tmp2}/fight-anim/fight_1.png")
-# print(tmp_file.get_height())
 
 idle_anim = [
     pygame.image.load(f"{dir_path}/idle-anim/idle_1.png"),
