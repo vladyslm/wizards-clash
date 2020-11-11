3c0ad6b390d86cb95847c00324e8987b7b331870
vladislav <vladislav.mpm@gmail.com>
2020-11-11 22:34:37 +0100

    modify: resized wiard's imgs

--- assets/gameassets/bluewizard/gameobj.py
+++ assets/gameassets/bluewizard/gameobj.py
@@ -16,13 +16,25 @@ fight_anim = [
     pygame.image.load(f"{dir_path}/fight-anim/fight_4.png"),
 ]
 
-height = idle_anim[0].get_height()
-width = idle_anim[0].get_width()
+
+# TODO: scale func should be an own module
+def scale(list_anim, a_width, a_height):
+    al = []
+    for frame in list_anim:
+        al.append(pygame.transform.scale(frame, (a_width, a_height)))
+    return al
+
+
+rescaled_fight_anim = scale(fight_anim, 218, 243)
+rescaled_idle_anim = scale(idle_anim, 218, 243)
+
+height = rescaled_fight_anim[0].get_height()
+width = rescaled_fight_anim[0].get_width()
 
 
 BLUE_WIZARD = {
-    "idle": idle_anim,
-    "fight": fight_anim,
+    "idle": rescaled_idle_anim,
+    "fight": rescaled_fight_anim,
     "height": height,
     "width": width
 }
