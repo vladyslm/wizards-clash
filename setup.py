b030baf20d19407fa75249e4de7f34246ad9ca7e
vladislav <vladislav.mpm@gmail.com>
2020-11-13 02:03:43 +0100

    create: setup module

---
+++ setup.py
@@ -0,0 +1,17 @@
+import pygame
+from configs.gameconf import SW, SH
+
+pygame.init()
+clock = pygame.time.Clock()
+
+SCREEN = pygame.display.set_mode((SW, SH))
+pygame.display.set_caption("WizardsClash")
+GREY = (211, 203, 202)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+font = pygame.font.SysFont("arial", 50, True)
+FONT_SMALL = pygame.font.SysFont("arial", 28, True)
+
+MENU_GB = pygame.image.load('assets/graphic/menu/wizards_clash.png')
+GAME_BG = pygame.image.load('./assets/graphic/background/BG1.png')
+SCOREBOARD_BG = pygame.image.load('assets/graphic/scoreboard/sboard_bg.png')
